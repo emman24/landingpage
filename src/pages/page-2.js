816cdbe1a3f00bbcf0ad1e9f55ce3e47a28c0780
emman <clinicstandard.csd7@gmail.com>
2019-03-26 11:28:01 +0800

    tada

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,20 +4,37 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = (props) => (
-  <Layout>
-		{console.log(props)}
-    <SEO title="Page two" />
-			<div className="container">
-				<div className="row">
-					<div className="twelve column">
-					<h1>{props.location.state.articlesData.title}</h1>
-					<p>{props.location.state.articlesData.body}</p>
-					<Link to="/">Go back</Link>
+const SecondPage = (props) => {
+ if(props.location.state !== null) {
+		return(
+		<Layout>
+			<SEO title="Articles" />
+				<div className="container">
+					<div className="row">
+						<div className="twelve column">
+						<h1>{props.location.state.articlesData.title }</h1>
+						<p>{props.location.state.articlesData.body}</p>
+						<Link to="/">Go back</Link>
+						</div>
 					</div>
 				</div>
-			</div>
-  </Layout>
-)
+		</Layout>
+		)
+ }else{
+	return(
+		<Layout>
+			<SEO title="Articles" />
+				<div className="container">
+					<div className="row">
+						<div className="twelve column">
+						<h1>Not Found</h1>
+						<Link to="/">Go back</Link>
+						</div>
+					</div>
+				</div>
+		</Layout>
+		)
+ }
+}
 
 export default SecondPage
