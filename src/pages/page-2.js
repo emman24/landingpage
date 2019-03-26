4b3832a22a2df226455e6e2074127abac9e1ac25
emman <clinicstandard.csd7@gmail.com>
2019-03-26 11:36:21 +0800

    add

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,40 +1,41 @@
-import React from "react"
+import React, { Component } from 'react'
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = (props) => {
- if(props.location.state !== null) {
-		return(
-		<Layout>
-			<SEO title="Articles" />
-				<div className="container">
-					<div className="row">
-						<div className="twelve column">
-						<h1>{props.location.state.articlesData.title }</h1>
-						<p>{props.location.state.articlesData.body}</p>
-						<Link to="/">Go back</Link>
-						</div>
-					</div>
-				</div>
-		</Layout>
-		)
- }else{
-	return(
-		<Layout>
-			<SEO title="Articles" />
-				<div className="container">
-					<div className="row">
-						<div className="twelve column">
-						<h1>Not Found</h1>
-						<Link to="/">Go back</Link>
-						</div>
-					</div>
-				</div>
-		</Layout>
-		)
- }
-}
 
-export default SecondPage
+export default class SecondPage extends Component {
+	constructor(props){
+		super(props)
+
+		this.state ={
+			articles:[]
+		}
+	}
+
+	componentDidMount() {
+		if(this.props.location.state !== null) {
+			this.setState({articles:this.props.location.state.articlesData})
+		}else {
+			this.setState({articles:{body:'Not Found', title:'Not Found'}})
+		}
+	}
+
+	render() {
+		return (
+			<Layout>
+			<SEO title="Articles" />
+				<div className="container">
+					<div className="row">
+						<div className="twelve column">
+						<h1>{this.state.articles.title}</h1>
+						<p>{this.state.articles.body}</p>
+						<Link to="/">Go back</Link>
+						</div>
+					</div>
+				</div>
+		</Layout>
+		)
+	}
+}
